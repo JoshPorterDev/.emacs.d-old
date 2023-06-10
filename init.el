68e2616f57dbd5e7505e6dd279243d9aa13d5739
Josh Porter <josh@joshporter.xyz>
2023-06-10 00:12:03 -0500

    Add counsel and ivy

--- init.el
+++ init.el
@@ -46,6 +46,35 @@
 (require 'evil)
 (evil-mode 1)
 
+;; Counsel
+(use-package counsel
+             :bind(("M-x" . counsel-M-x)
+                   ("C-x b" . counsel-ibuffer)
+                   ("C-x C-f" . counsel-find-file)
+                   :map minibuffer-local-map
+                   ("C-r" . counsel-minibuffer-history))
+             :config
+             (setq ivy-initial-inputs-alist nil))
+
+;; Ivy
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
