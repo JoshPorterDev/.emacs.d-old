362e9afa859e233d0a7a6c800a4260020b8e9f76
Josh Porter <josh@joshporter.xyz>
2023-06-09 23:29:28 -0500

    Add MELPA package archive and use-package

--- init.el
+++ init.el
@@ -12,3 +12,31 @@
 ;; Better scrolling
 (setq scroll-step 1)
 (setq scroll-conservatively 101)
+
+;; MELPA package archive
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Use-package utility
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
