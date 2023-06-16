889343ce6ba15d18767998b53b60375f3dec8809
Josh Porter <josh@joshporter.xyz>
2023-06-15 21:37:26 -0500

    Add minimal lsp and completion configuration

--- config.el
+++ config.el
@@ -65,6 +65,35 @@
   (setq dashboard-startup-banner 'logo)
   (setq dashboard-banner-logo-title "Josh's Emacs"))
 
+(use-package eglot)
+
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                    ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `corfu-exclude-modes'.
+  :init
+  (global-corfu-mode)
+  (corfu-popupinfo-mode))
+
+(use-package rust-mode)
+
 (use-package evil
   :init
   (setq evil-want-integration t)
