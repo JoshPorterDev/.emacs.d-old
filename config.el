1d3442a6d107578bac77412af4b4dce46eb28590
Josh Porter <josh@joshporter.xyz>
2023-07-03 23:01:11 -0500

    Change dashboard options

--- config.el
+++ config.el
@@ -63,7 +63,12 @@
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-startup-banner 'logo)
-  (setq dashboard-banner-logo-title "Josh's Emacs"))
+  (setq dashboard-banner-logo-title "Josh's Emacs")
+	(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5))))
 
 (use-package eglot)
 
